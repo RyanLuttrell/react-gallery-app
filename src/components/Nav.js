5848de8749168ee12b71f0a9e9b888461f39f23f
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-08-22 17:28:42 -0600

    Let's start this again

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,23 +6,10 @@ import {
     Switch,
     NavLink
 } from 'react-router-dom';
-import PhotoContainer from './PhotoContainer';
+
 
 const Nav = () => {
     return(
-        <BrowserRouter>
-            <nav class="main-nav">
-                <NavLink to='#'>Computers</NavLink>
-                <NavLink to='#'>Sunsets</NavLink>
-                <NavLink to='#'>Waterfalls</NavLink>
-
-                <Switch>
-                    <Route exact path="/" component={PhotoContainer} />
-                    <Route exact path="/" component={PhotoContainer} />
-                    <Route exact path="/" component={PhotoContainer} />
-                </Switch>
-            </nav>
-        </BrowserRouter>
 
     );
 }
